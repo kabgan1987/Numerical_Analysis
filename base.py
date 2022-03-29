3853f552da2c869460322d18d7f0dd8649b5d360
kabgan1987 <102479904+kabgan1987@users.noreply.github.com>
2022-03-29 22:33:26 +0430

    convert base in python
    Using this code you can convert your number
    in Decimal to other base system. Output will be a vector.
    author: Alireza Kabgani
    e-mail: a.kabgani@gmail.com

---
+++ base.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[82]:
+
+
+import numpy as np
+
+def base():
+    """
+    Using this code you can convert your number
+    in Decimal to other base system. Output will be a vector.
+    author: Alireza Kabgani
+    e-mail: a.kabgani@gmail.com
+    """
+    a1 = int(input("Enter your number in the decimal base: "))
+    b = int(input ("Enter the base number: "))
+    i=1
+    a=[a1]
+    qu=[]
+    r=[]
+    while True:
+        r.append(a[-1]%b)      
+        qu.append((a[-1]-r[-1])/b)
+        if qu[-1]<b:
+            break
+        a.append(int(qu[-1]))
+    r.append(int(qu[-1]))
+    return r
+r=base()
+d=r[::-1]
+print(f"You have entered {a1} and the base is {b}. The resukt is {d}")
+
